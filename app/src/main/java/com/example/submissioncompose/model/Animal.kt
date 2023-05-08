dca3695cf38018c5530cf7bc295dfca033de58a6
Rzldo <ezas4531@gmail.com>
2023-05-08 12:21:21 +0700

    initial commit

---
+++ app/src/main/java/com/example/submissioncompose/model/Animal.kt
@@ -0,0 +1,8 @@
+package com.example.submissioncompose.model
+
+data class Animal (
+    val id : Int,
+    val animalName : String,
+    val photoUrl : String,
+    val description : String
+    )
---
+++ app/src/main/java/com/example/submissioncompose/model/AnimalData.kt
@@ -0,0 +1,66 @@
+package com.example.submissioncompose.model
+
+object AnimalData {
+    val animal = listOf(
+        Animal(
+            1,
+            "Harimau",
+            "https://awsimages.detik.net.id/community/media/visual/2022/01/20/collarwali-mengenang-harimau-betina-super-dari-india.jpeg?w=700&q=90",
+            "Karnivora besar, lincah, kuat, berotot, predator, mematikan, soliter, terancam punah, liar."
+        ),
+        Animal(
+            2,
+            "Panda",
+            "https://asset.kompas.com/crops/CLKOgydea1KEwTYuz1rOrExk1hM=/0x36:1117x781/750x500/data/photo/2021/10/05/615c5f08bd530.jpg",
+            "Omnivora, gemuk, lucu, berbulu lembut, khas Cina, terancam punah"
+        ),
+        Animal(
+            3,
+            "Simpanse",
+            "https://asset-a.grid.id/crop/0x0:0x0/x/photo/2021/10/16/hewan-terhidrasijpg-20211016100104.jpg",
+            "Primates cerdas, kuat, terkait, sosial, pendaki, buah-buahan, liar"
+        ),
+        Animal(
+            4,
+            "Singa",
+            "https://www.kelaspintar.id/blog/wp-content/uploads/2022/03/makanan-hewan.jpg",
+            "Karnivora besar, gagah, berani, pemimpin, buas, teritorial, lincah, sosial, afrika."
+        ),
+        Animal(
+            5,
+            "Komodo",
+            "https://cdn-u1-gnfi.imgix.net/post/large-komodo-8be6b68b3f39b0b7c16a825e8e7bb7c8.jpg?fit=crop&crop=faces%2Centropy&lossless=true&auto=compress%2Cformat&w=390&h=260",
+            "Reptil raksasa, berbisa, terancam, pemangsa, pengintai, kuat, pulau."
+        ),
+        Animal(
+            6,
+            "Merak",
+        "https://cdn.antaranews.com/cache/730x487/2016/09/20160927Burung_Merak_Hijau.jpg",
+            "Burung indah, berwarna-warni, merajalela, berbulu indah, anggun, sulit ditangkap."
+        ),
+        Animal(
+            7,
+            "Badak",
+            "https://akcdn.detik.net.id/visual/2014/10/02/305bc082-b11e-4323-a848-4223e06ec0f3_169.jpg?w=650&q=90",
+            "Herbivora besar, bertanduk, pendiam, melindungi diri, kuat, terancam."
+        ),
+        Animal(
+            8,
+            "Serigala",
+            "https://cdn1-production-images-kly.akamaized.net/gB2MlHkzMdyVQplKhlFQ6eR2HgE=/1200x675/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4282969/original/029111900_1672931135-milo-weiler-1AIYdIb3O5M-unsplash.jpg",
+            "Karnivora sosial, cerdas, predator, penjaga, lincah, teritorial, malam."
+        ),
+        Animal(
+            9,
+            "Kucing",
+            "https://ugm.ac.id/galleries/crop/14977--730x420px.jpg",
+            "Hewan peliharaan, lincah, manis, imut, pemangsa, mandiri, penjilat."
+        ),
+        Animal(
+            10,
+            "Zebra",
+            "https://awsimages.detik.net.id/community/media/visual/2019/09/24/4ea59b38-9424-4792-8ee0-1d8399d4a748.jpeg?w=700&q=90",
+            "Herbivora berstrip, anggun, cepat, kawanan, teritorial, afrika."
+        )
+    )
+}
